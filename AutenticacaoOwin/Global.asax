f517089106c2899841fe80d8a201581c8794de95
Gladson Santos <gladsonbruno16@gmail.com>
2017-11-15 11:35:23 -0200

    Configurando tokens de acesso

--- AutenticacaoOwin/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutenticacaoOwin.WebApiApplication" Language="C#" %>
--- AutenticacaoOwin/Startup.cs
+++ AutenticacaoOwin/Startup.cs
@@ -3,6 +3,8 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
+using System.Web.Cors;
+using Microsoft.Owin.Cors;
 
 
 //[assembly: OwinStartup(typeof(AutenticacaoOwin.Startup))]
@@ -20,7 +22,39 @@ namespace AutenticacaoOwin
             routeTemplate: "api/{controller}/{id}",
             defaults: new { id = RouteParameter.Optional }
             );
+            //Usando configuração personalizada do cors
+            ConfigureCors(app);
+
+            //Ativando tokens de acesso
+            AtivandoAccessToken(app);
+
             app.UseWebApi(config);
+        }
+
+        private void AtivandoAccessToken(IAppBuilder app)
+        {
+
+        }
+
+        //Configuração personalizada do cors
+        private void ConfigureCors(IAppBuilder app)
+        {
+            //Definindo politica do cors
+            var politica = new CorsPolicy();
+            politica.AllowAnyHeader = true;
+            politica.Origins.Add("http://localhost:49774/");
+            politica.Origins.Add("http://localhost:49744/");
+            politica.Methods.Add("GET");
+            politica.Methods.Add("POST");
+
+            var corsOptions = new CorsOptions
+            {
+                PolicyProvider = new CorsPolicyProvider
+                {
+                    PolicyResolver = context => Task.FromResult(politica)
+                }
+            };
+            app.UseCors(corsOptions);
         }
     }
 }
